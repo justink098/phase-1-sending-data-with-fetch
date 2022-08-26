24450f6b557618cf3c89824f248f0ad409f64e85
Justin <justin.kiarie@student.moringaschool.com>
2022-08-26 16:11:41 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const p = document.createElement("p")
+
+function submitData (name, email){ 
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      }),
+    })
+      .then(resp => resp.json())
+      .then(object => {
+        console.log(object)
+        p.textContent = object.id
+        document.body.appendChild(p)
+      })
+      .catch( e =>{
+        p.textContent = e.message
+        document.body.appendChild(p)
+})
+}
+submitData("Steve","steve@steve.com")
